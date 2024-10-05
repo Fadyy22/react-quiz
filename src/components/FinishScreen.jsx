60afd4ca101ea724abf2cb0d307c0cf584d00e0d
Fadyy22 <fadyalaa441@gmail.com>
2024-10-05 19:20:00 +0300

    react-quiz app

---
+++ src/components/FinishScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function FinishScreen({ points, maxPoints, highscore, dispatch }) {
+  const percentage = (points / maxPoints) * 100;
+
+  return (
+    <>
+      <p className='result'>
+        You scored <strong>{points}</strong> out of {maxPoints} points (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className='highscore'>(Highscore: {highscore} points)</p>
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'restart' })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
